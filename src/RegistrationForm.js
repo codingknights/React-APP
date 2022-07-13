2b6e9cc97ca9247ef1f61de60ef00ca395b0847c
Andrews Sharvin <andrewssharvin05@gmail.com>
2022-07-13 17:13:10 +0400

    fetch url changed

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -76,7 +76,7 @@ const RegistrationForm = () => {
             formData.append('phoneNumber', phoneNumberField.value);
 
             fetch(
-                `${process.env.REACT_APP_BACKEND_ENDPOINT}/users/create`,
+                `${process.env.REACT_APP_BACKEND_ENDPOINT}`,
                 {
                     method: 'POST',
                     body: formData
