4e8ef6ccbaae2107daeac20dd9f3a4bba8680388
Andrews Sharvin <andrewssharvin05@gmail.com>
2022-07-13 18:21:32 +0400

    user/create route has been added

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -76,7 +76,7 @@ const RegistrationForm = () => {
             formData.append('phoneNumber', phoneNumberField.value);
 
             fetch(
-                `${process.env.REACT_APP_BACKEND_ENDPOINT}`,
+                `${process.env.REACT_APP_BACKEND_ENDPOINT}users/create`,
                 {
                     method: 'POST',
                     body: formData
